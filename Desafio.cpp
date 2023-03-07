239aaa07ef8d5f8b890f1ae25f7927647b4dc68a
Leonardo Romeiro <123381157+LeozinRomeiro@users.noreply.github.com>
2023-03-07 19:49:42 -0300

    Add files via upload

---
+++ Desafio.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+main(){
+	int serie,numero;
+	for(serie=1;serie<=1;serie=serie+1){
+		for(numero=1;numero<=500;numero=numero+1){
+			printf("Serie: %d - Numero: %d\n",serie,numero);
+		}
+	}
+	system("pause");
+	for(serie=2;serie<=2;serie=serie+1){
+		for(numero=501;numero<=1000;numero=numero+1){
+			printf("Serie: %d - Numero: %d\n",serie,numero);
+		}
+	}
+	system("pause");
+	for(serie=3;serie<=3;serie=serie+1){
+		for(numero=1001;numero<=2000;numero=numero+1){
+			printf("Serie: %d - Numero: %d\n",serie,numero);
+		}
+	}
+}
